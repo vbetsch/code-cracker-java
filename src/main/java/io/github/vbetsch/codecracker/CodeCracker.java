21df9be86ac1dc9aa9194d79e253978d01b03a9d
vbetsch <vic.betsch@gmail.com>
2026-06-30 22:51:35 +0200

    refactor: create Cypher class

--- src/main/java/io/github/vbetsch/codecracker/CodeCracker.java
+++ src/main/java/io/github/vbetsch/codecracker/CodeCracker.java
@@ -6,50 +6,14 @@ import java.util.Map;
 import static java.util.Map.entry;
 
 public class CodeCracker {
-
-    private final Map<Character, Character> dictionary = Map.ofEntries(
-            entry('a', '!'),
-            entry('b', ')'),
-            entry('c', '"'),
-            entry('d', '('),
-            entry('e', '£'),
-            entry('f', '*'),
-            entry('g', '%'),
-            entry('h', '&'),
-            entry('i', '>'),
-            entry('j', '<'),
-            entry('k', '@'),
-            entry('l', 'a'),
-            entry('m', 'b'),
-            entry('n', 'c'),
-            entry('o', 'd'),
-            entry('p', 'e'),
-            entry('q', 'f'),
-            entry('r', 'g'),
-            entry('s', 'h'),
-            entry('t', 'i'),
-            entry('u', 'j'),
-            entry('v', 'k'),
-            entry('w', 'l'),
-            entry('x', 'm'),
-            entry('y', 'n'),
-            entry('z', 'o')
-    );
-
-    private static Map<Character, Character> invertMap(Map<Character, Character> map) {
-        Map<Character, Character> inverted = new HashMap<>();
-        for (Map.Entry<Character, Character> entry : map.entrySet()) {
-            inverted.put(entry.getValue(), entry.getKey());
-        }
-        return inverted;
-    }
+    private final Cypher cypher = new Cypher();
 
     public char decryptLetter(char letter) {
-        return dictionary.get(letter);
+        return cypher.decryptLetter(letter);
     }
 
     public char encryptChar(char character) {
-        return invertMap(dictionary).get(character);
+        return cypher.encryptChar(character);
     }
 
     public String decryptWord(String string) {
