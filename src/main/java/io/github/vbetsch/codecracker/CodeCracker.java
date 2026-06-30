b06a2f939d12a3bb43f118b30876bb157cffc0f0
vbetsch <vic.betsch@gmail.com>
2026-06-30 21:42:25 +0200

    feat: create CodeCracker class and test with

---
+++ src/main/java/io/github/vbetsch/codecracker/CodeCracker.java
@@ -0,0 +1,5 @@
+package io.github.vbetsch.codecracker;
+
+public class CodeCracker {
+
+}
---
+++ src/test/java/io/github/vbetsch/codecracker/CodeCrackerTest.java
@@ -0,0 +1,16 @@
+package io.github.vbetsch.codecracker;
+
+import org.junit.jupiter.api.Test;
+
+class CodeCrackerTest {
+
+    @Test
+    void should_doSomething_whenSomeCondition() {
+        // Arrange
+        CodeCracker codeCracker = new CodeCracker();
+
+        // Act
+        // Assert
+    }
+
+}
