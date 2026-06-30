e58389542ff5c14bd3a94ec6463c7700da8e3996
vbetsch <vic.betsch@gmail.com>
2026-06-30 22:29:43 +0200

    refactor: remove static statement

--- src/main/java/io/github/vbetsch/codecracker/CodeCracker.java
+++ src/main/java/io/github/vbetsch/codecracker/CodeCracker.java
@@ -3,16 +3,17 @@ package io.github.vbetsch.codecracker;
 import java.util.Map;
 
 public class CodeCracker {
-    private static final Map<Character, Character> translations = Map.of(
+
+    private final Map<Character, Character> translations = Map.of(
             'a', '!',
             'b', ')'
     );
 
-    public static char decryptLetter(char letter) {
+    public char decryptLetter(char letter) {
         return translations.get(letter);
     }
 
-    public static String decryptWord(String string) {
+    public String decryptWord(String string) {
         StringBuilder stringBuilder = new StringBuilder();
         for (char c : string.toCharArray()) {
             stringBuilder.append(decryptLetter(c));
